b2e5bcd6f0732ecc69d3669d2a10b44572d0136d
zaiaaa <gustavo.0801214@gmail.com>
2024-03-19 08:55:38 -0300

    riscando os desafios feitos

--- Atividade/Revisao.js
+++ Atividade/Revisao.js
@@ -1,13 +1,13 @@
-1) Escreva um programa em JavaScript que apresente uma caixa de alerta para o usuário com uma informação qualquer.
+// TODO 1) Escreva um programa em JavaScript que apresente uma caixa de alerta para o usuário com uma informação qualquer.
 
-2) Crie um  script em que o usuário clicará com o botão e em seguida uma mensagem de alerta será apresentada com uma mensagem qualquer.
+// TODO 2) Crie um  script em que o usuário clicará com o botão e em seguida uma mensagem de alerta será apresentada com uma mensagem qualquer.
 
-3)  De exemplo dos seguintes formas de trabalhar com DOM(Document Object Model), e explique:
-     . document.getElementById();
-     . document.getElementsByTagName();
-     . document.getElementsByClassName();
-     . document.querySelector();
-     . document.querySelectorAll();
+// 3)  De exemplo dos seguintes formas de trabalhar com DOM(Document Object Model), e explique:
+//     . document.getElementById();
+//    . document.getElementsByTagName();
+//     . document.getElementsByClassName();
+//     . document.querySelector();
+//     . document.querySelectorAll();
 
 4) Descreva e crie exemplos com suas formas diferente eventos no javascript,segue alguns exemplo:
   . onmouseover
